2ba99d9d4fe4cf9bc6d7d8c7cdc26573e99cbb99
dcdegithub <l>
2019-02-19 09:51:35 +0800

    权限列表完成

--- src/components/rights.vue
+++ src/components/rights.vue
@@ -1,6 +1,23 @@
 <template>
 <el-card>
     <cus-bread level1="权限管理" level2="权限列表"></cus-bread>
+
+    <!--  表格-->
+    <!-- 表格 -->
+    <el-table height="500px" :data="list" style="width: 100%">
+      <el-table-column type="index" label="#" width="80"></el-table-column>
+      <el-table-column prop="authName" label="权限名称" width="180"></el-table-column>
+      <el-table-column prop="path" label="路径" width="180"></el-table-column>
+
+      <el-table-column  label="层级" width="180">
+        <template slot-scope="scope">
+            <span v-if="scope.row.level==='0'">一层</span>
+            <span v-if="scope.row.level==='1'">二层</span>
+            <span v-if="scope.row.level==='2'">三层</span>
+        </template>
+      </el-table-column>
+    </el-table>
+    
 </el-card>
   
 </template>
